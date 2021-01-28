3995db68650ee92eee3c8cb978582cdab211cadb
jinyi <1119510658@qq.com>
2021-01-28 16:39:36 +0800

    test for commit

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,135 @@
+public class ArrayDeque<I> {
+    private int size;
+    private int nextFirst;
+    private int nextLast;
+    private I[] items = (I[]) new Object[3];
+
+    public ArrayDeque() {
+        nextFirst = items.length - 1;
+        nextLast = 0;
+        size = 0;
+    }
+
+    public void downsize() {
+        if (4 * size < items.length) {
+            I[] a =(I[]) new Object[items.length / 2];
+            copyresize(items, a);
+        }
+
+    }
+
+    public void copyresize(I[] i, I[] a) {
+        System.arraycopy(i, nextFirst + 1, a, 0, i.length - nextFirst - 1);
+        System.arraycopy(i, 0, a, i.length - nextFirst - 1, nextLast);
+        items = a;
+        nextLast = i.length;
+        nextFirst = items.length - 1;
+    }
+
+    private void resize(int capacity) {
+        I[] a =(I[]) new Object[capacity];
+        copyresize(items, a);
+    }
+
+    public void addFirst(I x) {
+        if (size == items.length) {
+            resize(2 * items.length);
+        }
+
+        items[nextFirst] = x;
+        nextFirst--;
+        size++;
+    }
+
+    public void addLast(I x) {
+        if (size == items.length) {
+            resize(2 * size);
+        }
+
+        items[nextLast] = x;
+        nextLast++;
+        size++;
+    }
+
+    public I getLast() {
+        return items[nextLast - 1];
+    }
+
+    public I get(int i) {
+        if (i <= items.length - nextFirst - 2) {
+            return items[nextFirst + i + 1];
+        }
+        return items[i - (items.length - nextFirst - 1) - 1];
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public I removeFirst() {
+        I x;
+        if (nextFirst == items.length - 1) {
+            x = items[0];
+            I[] a =(I[]) new Object[items.length];
+            System.arraycopy(items, 1, a, 0, size - 1);
+            this.items = a;
+            --this.size;
+            --this.nextLast;
+            this.downsize();
+            return x;
+        } else {
+            x = this.items[this.nextFirst + 1];
+            this.items[this.nextFirst + 1] = null;
+            --this.size;
+            ++this.nextFirst;
+            this.downsize();
+            return x;
+        }
+    }
+
+    public I removeLast() {
+        I x = this.getLast();
+        this.items[this.nextLast - 1] = null;
+        --this.size;
+        --this.nextLast;
+        this.downsize();
+        return x;
+    }
+
+    public void printDeque() {
+        int begin = this.nextFirst + 1;
+
+        Object var10001;
+        int end;
+        for(end = 0; begin < this.items.length; ++begin) {
+            var10001 = this.items[begin];
+            System.out.print(var10001 + " ");
+        }
+
+        while(end < this.nextLast) {
+            var10001 = this.items[end];
+            System.out.print(var10001 + " ");
+            ++end;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        ArrayDeque<Integer> AD = new ArrayDeque();
+        AD.addFirst(5);
+        AD.addLast(9);
+        AD.addLast(8);
+        AD.addFirst(4);
+        AD.addFirst(3);
+        AD.printDeque();
+        AD.removeFirst();
+        AD.removeFirst();
+        AD.removeLast();
+        AD.removeFirst();
+    }
+}
+
