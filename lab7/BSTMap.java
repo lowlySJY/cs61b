198739c4c485fdd16ab8852287d2735d30335195
lowlySJY <1119510658@qq.com>
2022-01-08 18:57:10 +0800

    finish tasks without extra exercise

---
+++ lab7/BSTMap.java
@@ -0,0 +1,164 @@
+import java.security.Key;
+import java.util.Iterator;
+import java.util.Set;
+
+public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
+    private Node root;
+
+    private class Node {
+        private K key;
+        private V value;
+        private Node left, right;
+        private int size;
+
+        public Node(K key, V value, int size) {
+            this.key = key;
+            this.value = value;
+            this.size = size;
+        }
+    }
+
+    @Override
+    public void clear() {
+        root = null;
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        if (key == null) {
+            throw new IllegalArgumentException("argument to containsKey() is null");
+        }
+        return (get(key) != null);
+    }
+
+    @Override
+    public V get(K key) {
+        if (key == null) {
+            throw new IllegalArgumentException("calls get() with a null key");
+        }
+        if (root == null) {
+            return null;
+        } else {
+            return get(root, key);
+        }
+    }
+
+    private V get(Node x, K key) {
+        if (x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            return get(x.left, key);
+        } else if (cmp > 0) {
+            return get(x.right, key);
+        } else {
+            return x.value;
+        }
+    }
+
+    @Override
+    public int size() {
+        return size(root);
+    }
+
+    private int size(Node x) {
+        if (x == null) {
+            return 0;
+        } else {
+            return x.size;
+        }
+    }
+
+    @Override
+    public void put(K key, V value) {
+        if (key == null) {
+            throw new IllegalArgumentException("calls put() with a null key");
+        }
+        if (value == null) {
+            remove(key);
+        } else {
+            root = put(root, key, value);
+        }
+    }
+
+    private Node put(Node x, K key, V val) {
+        if (x == null) {
+            return new Node(key, val,1);
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            x.left = put(x.left, key, val);
+        } else if (cmp > 0) {
+            x.right = put(x.right, key, val);
+        } else {
+            x.value = val;
+        }
+        x.size = 1 + size(x.left) + size(x.right);
+        return x;
+    }
+
+    @Override
+    public Set keySet() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public V remove(K key) {
+//        throw new UnsupportedOperationException();
+        if (key == null) {
+            throw new IllegalArgumentException("calls remove() with a null key");
+        }
+        V val = get(key);
+        root = remove(root, key);
+        return val;
+    }
+
+    private Node remove(Node x, K key) {
+        if (x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            x.left = remove(x.left, key);
+        } else if (cmp > 0 ) {
+            x.right = remove(x.right, key);
+        } else {
+            if (x.left == null) return x.right;
+            if (x.right == null) return x.left;
+            Node t = x;
+            x = min(t.right);
+            x.right = removeMin(t.right);
+            x.left = t.left;
+        }
+        x.size = size(x.left) + size(x.right);
+        return x;
+    }
+
+    private Node min(Node x) {
+        if (x.left == null) {
+            return x;
+        } else {
+            return min(x.left);
+        }
+    }
+
+    private Node removeMin(Node x) {
+        if (x.left == null) {
+            return x.right;
+        }
+        x.left = removeMin(x.left);
+        x.size = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+
+    @Override
+    public V remove(K key, V value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Iterator iterator() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void printInOrder() {
+    }
+}
+
